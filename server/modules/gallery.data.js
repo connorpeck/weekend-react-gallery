f6394bdb7bfeb3122f4566bb95170a1c78072fc0
connorpeck <90285369+connorpeck@users.noreply.github.com>
2022-06-06 11:17:11 -0500

    put route connected, like button on dom

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -10,6 +10,18 @@ const galleryItems = [
     { id: 3, path: 'images/', 
     title: 'Hal',
     description: 'Wow!.', 
+    likes: 0 },
+    { id: 4, path: 'images/myself.jpg', 
+    title: 'Myself',
+    description: 'Photo of myself taken near lake Superior.', 
+    likes: 0 },
+    { id: 5, path: 'images/', 
+    title: 'My cute dog',
+    description: 'Photo of Benji!.', 
+    likes: 0 },
+    { id: 6, path: 'images/', 
+    title: 'Hal',
+    description: 'Wow!.', 
     likes: 0 }
 ];
 
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 function GalleryItem( props ){
     const [ show, setShow ] = useState( true );
 
     const toggleShow = () => {
         setShow (!show);
-    }
+    } // end toggleShow
+
+    const likeGalleryItem = () => {
+        // console.log(props.galleryItem.id);
+        axios.put(('./gallery/like/:id'), 
+        {id: props.galleryItem.id}).then((response) => {
+            console.log(response.data);
+          });
+      }
     return (
+        <div>
         <div onClick={toggleShow} className="galleryItem">
             {
             show?
@@ -15,7 +25,15 @@ function GalleryItem( props ){
             :
             <h2>{props.galleryItem.description}</h2>
 
-            }
+            } 
+
+
+
+            
+            
+        </div>
+        <button id={props.galleryItem.id} onClick={likeGalleryItem}>like</button>
+        <p>likes: {props.galleryItem.likes}</p>
         </div>
     );
 }
